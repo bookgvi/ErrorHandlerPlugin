7f86703a0c4ea75bbeb341fbff5bf8c8f2865b1c
GitHub Action <action@github.com>
2024-02-16 20:45:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/bookgvi/errorhandlerplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bookgvi/errorhandlerplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.bookgvi.errorhandlerplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bookgvi.errorhandlerplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
