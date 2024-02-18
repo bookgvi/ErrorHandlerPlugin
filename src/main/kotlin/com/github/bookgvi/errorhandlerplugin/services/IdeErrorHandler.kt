d03b3c0b52c280e0ae9cc24c7c4efc18133ff22a
bookgvi <bookgvi@gmail.com>
2024-02-18 11:24:13 +0300

    error report submitter

--- src/main/kotlin/com/github/bookgvi/errorhandlerplugin/services/IdeErrorHandler.kt
+++ src/main/kotlin/com/github/bookgvi/errorhandlerplugin/services/IdeErrorHandler.kt
@@ -1,5 +1,6 @@
 package com.github.bookgvi.errorhandlerplugin.services
 
+import com.github.bookgvi.errorhandlerplugin.ErrorHandlerBundle
 import com.intellij.diagnostic.MessagePool
 import com.intellij.diagnostic.MessagePoolListener
 import com.intellij.ide.AppLifecycleListener
@@ -9,6 +10,8 @@ import com.intellij.openapi.ui.Messages
 import com.jetbrains.rd.util.getThrowableText
 
 class IdeErrorHandler : AppLifecycleListener {
+
+    private val ERROR_TITLE : String = ErrorHandlerBundle.message("error.handler.window.title.text")
     override fun appFrameCreated(commandLineArgs: MutableList<String>) {
         super.appFrameCreated(commandLineArgs)
         MessagePool.getInstance().addListener(
@@ -21,7 +24,7 @@ class IdeErrorHandler : AppLifecycleListener {
                             if (pluginId != null) {
                                 val errorMsg = String.format("Error occured %s:\n %s", pluginId.idString, t.getThrowableText())
                                 println(errorMsg)
-                                ApplicationManager.getApplication().invokeLater { Messages.showInfoMessage(errorMsg, "ERROR") }
+                                ApplicationManager.getApplication().invokeLater { Messages.showInfoMessage(errorMsg, ERROR_TITLE) }
                                 msg.isRead = true;
                             }
                         }
@@ -30,7 +33,7 @@ class IdeErrorHandler : AppLifecycleListener {
                     override fun entryWasRead() {
                         val msg = "Error has been read"
                         println(msg)
-                        ApplicationManager.getApplication().invokeLater { Messages.showInfoMessage(msg, "ERROR WAS READ") }
+                        ApplicationManager.getApplication().invokeLater { Messages.showInfoMessage(msg, ERROR_TITLE) }
                     }
                 }
         )
